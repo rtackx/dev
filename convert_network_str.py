5e6df26fca35b7761dc5ea7bc2e2da81720520dd
n3.wool <test@test.com>
2016-09-15 15:07:20 +0200

    Update 2nd 09/15/16

---
+++ convert_network_str.py
@@ -0,0 +1,38 @@
+import sys
+
+filename_network = sys.argv[1]
+nodes = {}
+
+index = 0
+
+with open(filename_network, 'r') as file:
+	for line in file:
+		line = line.replace("\n", "").split()
+
+		index_node1 = line[0]
+		index_node2 = line[1]
+
+		if index_node1 not in nodes:
+			nodes[index_node1] = index
+			index += 1
+		if index_node2 not in nodes:
+			nodes[index_node2] = index
+			index += 1
+
+f = open(filename_network + '_table_num', 'w')
+for index in nodes:
+	f.write(index + ' ' + str(nodes[index]) + '\n')
+f.close()
+
+f = open(filename_network + '_num', 'w')
+with open(filename_network, 'r') as file:
+	for line in file:
+		line = line.replace("\n", "").split()
+
+		index_node1 = line[0]
+		index_node2 = line[1]
+
+		f.write(nodes[index_node1] + ' ' + nodes[index_node2] + '\n')
+
+f.close()
+
